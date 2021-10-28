faa68adad2cf72971f3a7aef24e622fd696c05b9
Sarah <sarahk1280@gmail.com>
2021-10-28 18:56:39 -0400

    feat(login): add login form and basic authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { OverrideMaterialUICss } from 'override-material-ui-css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <OverrideMaterialUICss>
+      <App />
+    </OverrideMaterialUICss>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,6 +1,120 @@
-import React from 'react';
-import './styles/login.css';
+import React, { useState } from 'react';
+import { Button, TextField } from '@mui/material';
+import { css } from '@emotion/css';
 
-const Login = () => <div>Login</div>;
+const InputBox = ({ label, value, handleChange }) => (
+  <TextField
+    id="outlined-name"
+    margin="dense"
+    label={label}
+    value={value}
+    onChange={(e) => handleChange(e, label)}
+  />
+);
+
+const Login = ({ isLoggedIn }) => {
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [users, setUsers] = useState([
+    {
+      username: 'sarah',
+      password: 'sarah',
+    },
+  ]);
+  const [input, setInput] = useState([
+    { name: 'Username', value: 'sds' },
+    { name: 'Password', value: 'asd' },
+  ]);
+
+  const handleChange = (e, name) =>
+    setInput(
+      input.map((item) => {
+        if (item.name === name) {
+          item.value = e.target.value;
+        }
+        return item;
+      }),
+    );
+
+  const isValidUser = () => {
+    const usernameInput = input.find((item) => item.name === 'Username');
+    const passwordInput = input.find((item) => item.name === 'Password');
+    const user = users.find((u) => u.username === usernameInput.value);
+    if (!user) return false;
+    return user.password === passwordInput.value;
+  };
+
+  const authenticateUser = () => {
+    if (isValidUser()) {
+      console.log('valid!');
+    } else {
+      console.log('invalid!');
+    }
+  };
+
+  return (
+    <div
+      className={css`
+        width: 100%;
+        height: calc(100vh - 50px);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      `}
+    >
+      <div
+        className={css`
+          border: solid 1px grey;
+          border-radius: 15px;
+          display: flex;
+          flex-direction: column;
+          margin: 50px;
+          padding: 50px;
+          width: 500px;
+        `}
+      >
+        <h1 className="title">{isLoginMode ? 'Welcome back!' : 'New user!'}</h1>
+        <form
+          className={css`
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          {input.map((item) => (
+            <InputBox
+              label={item.name}
+              value={item.value}
+              handleChange={handleChange}
+            />
+          ))}
+          <div
+            className={css`
+              display: flex;
+              gap: 10px;
+              margin-top: 30px;
+              justify-content: center;
+            `}
+          >
+            <Button
+              className={css`
+                box-shadow: none;
+              `}
+              variant="contained"
+              onClick={authenticateUser}
+            >
+              {isLoginMode ? 'Login' : 'Signup'}
+            </Button>
+            <Button
+              variant="outlined"
+              onClick={() => setIsLoginMode(!isLoginMode)}
+              type="Button"
+            >
+              {`Switch to ${isLoginMode ? 'Sign up' : 'Log in'}`}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
