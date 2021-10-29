7a4ec00b561ecd623df72c62a7b11c9dc482d280
Sarah <sarahk1280@gmail.com>
2021-10-29 16:32:17 -0400

    feat: events action and reducer

--- src/views/HomePage/index.js
+++ src/views/HomePage/index.js
@@ -1,10 +1,16 @@
 import { connect } from 'react-redux';
 import { logout } from '../../actions/authActions';
+import { addAttendee, removeAttendee } from '../../actions/eventsActions';
 import HomePage from './HomePage';
 
 const mapStateToProps = (state) => ({
   isLoggedIn: state.auth.isLoggedIn,
   user: state.auth.user,
+  events: state.events,
 });
 
-export default connect(mapStateToProps, { logout })(HomePage);
+export default connect(mapStateToProps, {
+  logout,
+  addAttendee,
+  removeAttendee,
+})(HomePage);
