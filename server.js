8b4f77148725a66f9ad6ee03193209cc28ec87d7
Angelina Choo Woon Chee <angiecwc25@gmail.com>
2021-12-08 00:31:42 -0500

    Add webpage routes to server.js

--- server.js
+++ server.js
@@ -528,7 +528,15 @@ app.delete('/event-dashboard/:eventID/unattend', async (req, res) => {
 
 /** * Webpage routes below ********************************* */
 // Serve the build
-app.use(express.static(__dirname + "/client/build"));
+if (env === 'production') {
+  app.use(express.static(__dirname + "/client/build"));
+
+  // All routes other than above will go to index.html
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname + "/client/build/index.html");
+  });
+}
+
 
 /** ********************************************** */
 // Express server listening...
