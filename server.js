86044ed1b4c690ad6c0e322483762e223e5277c1
Angelina Choo Woon Chee <angiecwc25@gmail.com>
2021-12-07 23:13:09 -0500

    Heroku

--- server.js
+++ server.js
@@ -530,11 +530,6 @@ app.delete('/event-dashboard/:eventID/unattend', async (req, res) => {
 // Serve the build
 app.use(express.static(__dirname + "/client/build"));
 
-// // All routes other than above will go to index.html
-// app.get("*", (req, res) => {
-//     res.sendFile(__dirname + "/client/build/index.html");
-// });
-
 /** ********************************************** */
 // Express server listening...
 const port = process.env.PORT || 5001;
